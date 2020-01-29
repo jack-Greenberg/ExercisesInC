4e47043eaa6cd70817e68ef7b2f348a2d1ad8c13
Jack Greenberg <j.lester.greenberg@gmail.com>
2020-01-29 09:35:34 -0500

    Experimented with compiling and assembly language

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int x = 5;
+    int y = x + 1;
+
+    printf("%d", y);
     return 0;
 }
+
+/* Optimization
+1. The only additional code is
+```
+subq	$16, %rsp
+movl	$5, -4(%rbp)
+```
+and instead of `popq    %rbp`, I see `leave` in the version with x = 5.
+
+2. Adding -O2 causes the assembly code file to be shorter and removes some of the instructions.
+It shortens the .LFB0 section, and changes its name to .LFB23
+
+3. Printing x adds
+```
+movl	-4(%rbp), %eax
+movl	%eax, %esi
+```
+and changes the .string at the top from "Hello, World" to "%d". In the optimized assembly language
+the only addition is `movl	$5, %edx`.
+
+4. The unoptimized code adds 5 additional lines to the version where we printed x, but
+the optimized code only changes a single line, and the line `movl    $5, -4(%rbp)` changes the 5 to a 6.
+
+5. Optimization looks at the final results of variables and only includes information about
+their final state as needed by other functions. If we were to print both x and y, there
+would probably be additional information about in the optimized version.
+
+*/
