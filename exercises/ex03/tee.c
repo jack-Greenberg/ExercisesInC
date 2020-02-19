ad6c1ae7e86fe91e25df5092a881c1b7519c22aa
Jack Greenberg <j.lester.greenberg@gmail.com>
2020-02-18 23:44:45 -0500

    Write to stdout and file (buggy)
    Issue with writing to a file where the buffer somehow wraps and the file
    closes and opens again so that the first part that was written is over-
    written by the second part

--- exercises/ex03/tee.c
+++ exercises/ex03/tee.c
@@ -6,31 +6,39 @@ char *FILE_MODE = "w";
 
 int main(int argc, char *argv[])
 {
-    char buffer[255];
+    char buffer[255] = {};
     char ch;
     FILE *out;
-
-    // argv += 1; // First argv is the command called (./tee)
 
     while ((ch = getopt(argc, argv, "a")) != EOF) {
         switch(ch) {
             case 'a':
                 FILE_MODE = "a";
                 break;
-            case '?':
-                fprintf(stderr, "Unrecognized option: '-%c'\n", optopt);
             default:
+                fprintf(stderr, "usage: tee [-a] [file ...]\n");
                 exit(1);
-                break;
         }
     }
 
-    while (fscanf(stdin, "%s", buffer) != EOF) {
-        for (int i = 1; i < argc; i++) {
+    argc -= optind;
+    argv += optind;
+
+    size_t num_bytes_read;
+    while ((num_bytes_read = read(STDIN_FILENO, buffer, sizeof buffer)) > 0) {
+
+        for (int i = 0; i < argc; i++) {
+
             if (!(out = fopen(argv[i], FILE_MODE))) {
                 fprintf(stderr, "Can't open file: '%s'\n", argv[i]);
+                exit(1);
             }
-            fprintf(out, "%s", buffer);
+
+            fwrite(buffer, num_bytes_read, 1, out);
+            fclose(out);
         }
+
+        // Write to stdout
+        fwrite(buffer, num_bytes_read, 1, stdout);
     }
 }
